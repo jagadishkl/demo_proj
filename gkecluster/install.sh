0932efda7e8846708af62c258cdab0f9ed93c71b
Kl <Jagadish.Kl@emc.com>
2018-11-02 17:40:18 +0530

    changed

--- gkecluster/install.sh
+++ gkecluster/install.sh
@@ -1,1 +1,11 @@
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+# Set the Kubernetes version as found in the UCP Dashboard or API
+k8sversion=v1.8.11
+
+# Get the kubectl binary.
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$k8sversion/bin/darwin/amd64/kubectl
+
+# Make the kubectl binary executable.
+chmod +x ./kubectl
+
+# Move the kubectl executable to /usr/local/bin.
+sudo mv ./kubectl /usr/local/bin/kubectl
